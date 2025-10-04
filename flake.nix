d425cd01fd9835bd0920a35cd636456afc949b5e
Mateo <79337611+mateowoetam@users.noreply.github.com>
2025-10-04 19:40:10 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,29 @@
 {
-  description = "A Flake providing everything needed to run beheader";
+  description = "A Flake providing everything needed to run Beheader";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
-        f {
-          pkgs = import nixpkgs { inherit system; };
-          system = system;
-        }
-      );
-    in {
-      packages = forAllSystems ({ pkgs, ... }: {
-        default = pkgs.buildEnv {
-          name = "beheader-env";
-          paths = [
-            pkgs.ffmpeg
-            pkgs.imagemagick
-            pkgs.zip
-            pkgs.unzip
-            pkgs.bun
-            pkgs.bento4
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    cachix.url = "github:cachix/cachix";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      cachix,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = flake-utils.lib.forAllSystems (system: {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        configuration = {
+          environment.systemPackages = with nixpkgs.pkgs; [
+            ffmpeg
+            imagemagick
+            zip
+            unzip
+            bun
+            bento4
           ];
         };
       });
